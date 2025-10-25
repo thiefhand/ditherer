417bc1ca62e2d32ccd8b5677d9d77fa30d28c7fc
gusg21 <gus.goucher@gmail.com>
2025-10-25 02:44:16 -0400

    [algos] added sierra lite

---
+++ main_view/algos/sierra_lite.gd
@@ -0,0 +1,22 @@
+extends Algo
+class_name SierraLite
+
+@export_range(1, 10, 1) var steps: int = 1
+
+func get_display_name() -> String:
+	return "Sierra Lite"
+
+func dither(image: Image) -> Image:
+	var working_image = image.duplicate()
+	for x in range(image.get_width()):
+		for y in range(image.get_height()):
+			var src_color = working_image.get_pixel(x, y)
+			var quant_color = quantize_color(src_color, steps)
+			var error = src_color - quant_color
+
+			working_image.set_pixel(x, y, quant_color)
+			adjust_pixel(working_image, x + 1, y + 0, error * (2.0 / 4.0))
+			adjust_pixel(working_image, x - 1, y + 1, error * (1.0 / 4.0))
+			adjust_pixel(working_image, x + 0, y + 1, error * (1.0 / 4.0))
+	
+	return working_image
